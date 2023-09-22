52ea45c8cfb5de6c21ffe4c4a122328429308dc4
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-09-22 14:48:54 +0200

    [mod]:try with loop forEach

--- js/script.js
+++ js/script.js
@@ -10,20 +10,44 @@ let counter = 0;
 itemsCarousel.innerHTML = '';
 thumbnails.innerHTML = '';
 
+
+
+//      ARRAY OBJECTS 
 const images = [
-  'img/01.webp',
-  'img/02.webp',
-  'img/03.webp',
-  'img/04.webp',
-  'img/05.webp'
-]
+  {
+      image: 'img/01.webp',
+      title: 'Marvel\'s Spiderman Miles Morale',
+      text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+  }, {
+      image: 'img/02.webp',
+      title: 'Ratchet & Clank: Rift Apart',
+      text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+  }, {
+      image: 'img/03.webp',
+      title: 'Fortnite',
+      text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+  }, {
+      image: 'img/04.webp',
+      title: 'Stray',
+      text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+  }, {
+      image: 'img/05.webp',
+      title: "Marvel's Avengers",
+      text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+  }
+];
+//     /ARRAY OBJECTS
 
-for(let i = 0; i < images.length;i++){
-  const image = images[i];
-  itemsCarousel.innerHTML += `<img src="${image}" class= "newItem hide">`;
-  thumbnails.innerHTML += `<img src="${image}"class= "thumbItem inactive">`;
+images.forEach((element) => {
+  itemsCarousel.innerHTML += `
+  <div>
+  <img src = '${element.image}'> 
+  <h3>${element.title}</h3> 
+  <p>${element.text}</p>
+  </div>`
 
-}
+}) 
+
 
 const newItem = document.getElementsByClassName('newItem');
 
